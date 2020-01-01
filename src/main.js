4878a28ac961fa11428854adbe25e42c0f89ba11
e <daydream0839@outlook.com>
2020-01-02 01:07:41 +0800

    添加多语言切换

--- src/main.js
+++ src/main.js
@@ -5,11 +5,15 @@ import ViewUI from 'view-design';
 import 'view-design/dist/styles/iview.css';
 import global from '@/utils/global'
 import api from '@/http'
+import 'font-awesome/css/font-awesome.min.css'
+import i18n from './i18n'
+
 Vue.use(ViewUI);
 Vue.config.productionTip = false
 Vue.prototype.global = global
 Vue.prototype.$api = api
 new Vue({
-  router,
-  render: h => h(App),
+    i18n,
+    router,
+    render: h => h(App),
 }).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="page">
         <h2>Home Page</h2>
+        <h3>{{$t('common.doc')}}</h3>
         <Button type="primary" @click="testAxios()">Axios</Button>
+        <Button type="success" @click="changeLang('zh_cn')">简体中文</Button>
+        <Button type="success" @click="changeLang('en_us')">English</Button>
         <Button type="primary" @click="getUser()">用户</Button>
         <Button type="primary" @click="getMenu()">菜单</Button>
     </div>
@@ -10,6 +13,7 @@
 <script>
     import axios from 'axios'
     import mock from '@/mock/index'
+
     export default {
         name: "Home",
         data() {
@@ -30,6 +34,9 @@
                 axios.get('http://localhost:8080/menu').then(res => {
                     alert(JSON.stringify(res.data))
                 })
+            },
+            changeLang(lang) {
+                lang === '' ? 'zh_ch' : this.$i18n.locale = lang
             }
         },
         beforeCreate: function () {
